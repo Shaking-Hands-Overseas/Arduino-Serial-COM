a4d5bc545e26129a18d1b0f02c1d6f1d45448c72
Gondoraragorn <Joel.garcia1202@hotmail.com>
2022-01-11 17:41:12 +0100

    Updated mini mistake on run.ps1

--- run.ps1
+++ run.ps1
@@ -2,5 +2,6 @@ echo "Checking for updates..."
 py -m pip install --upgrade pip -q
 pip install pyserial -q
 pip install requests -q
-echo "Starting Arduino Serial COM...\n"
+echo "Starting Arduino Serial COM..."
+echo
 python main.py
