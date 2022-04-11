8c9e27fcbaf13721d74eefb41a14c92e51a3ebb7
Gondoraragorn <Joel.garcia1202@hotmail.com>
2022-04-11 18:23:45 +0200

    Added testing for jsonifying the content

--- Tests/Main_Tests.py
+++ Tests/Main_Tests.py
@@ -1,30 +1,34 @@
 import unittest
+import os
 from serial import Serial
-import json
+from json import loads, load
 from requests import get, post
 class test_sender(unittest.TestCase):
-    config = {"serial_port": "3", "Mode": "", "BAUDRATE": 9600,
-                   "Networking": {"API_URL": "", "URL_R": "/receiver",
-                                  "URL_S": "/servo", "URL_C": "/custom",
-                                  "DATA_TEMPLATE": {"s1": 200, "s2": 200, "s3": 200, "s4": 200, "s5": 200},
-                                  "PREFERRED_ORDER": {"F1": "s1", "F2": "s2", "F3": "s3", "F4": "s4", "F5": "s5"}}}
+    with open(os.path.join(os.getcwd(), "sho_config.json"), "r") as f:
+        config = load(f)
     Networking = config["Networking"]
-
     def test_ArduinoConnect(self):
         ard = Serial(port="COM4", baudrate=115200, timeout=.1)
         ard.write(bytes("A", 'utf-8'))
         ard.readline().decode('utf-8')
 
     def test_server_post_servo(self):
-        post(f'{self.Networking["API_URL"]}/custom/servo', self.Networking["DATA_TEMPLATE"])
+        post(f'{self.Networking["API_URL"]}/servo', self.Networking["DATA_TEMPLATE"])
 
     def test_server_post_custom(self):
         post(f'{self.Networking["API_URL"]}/custom', self.Networking["PREFERRED_ORDER"])
 
     def test_server_get(self):
-        x = get(f'{self.Networking["API_URL"]}/receiver')
-        self.ct = json.loads(x.content.decode())
+        get(f'{self.Networking["API_URL"]}/receiver')
 
+    def test_jsonfy(self):
+        _dict = loads(get(f'{self.Networking["API_URL"]}/receiver').content.decode())
+        for key, value in _dict.items():
+            if len(str(int(value))) == 1:
+                _dict[key] = "00" + str(int(value))
+            elif len(str(int(value))) == 2:
+                _dict[key] = "0" + str(int(value))
+        To_send = "".join(str(x) for x in _dict.values())
 
 if __name__ == '__main__':
     unittest.main()
